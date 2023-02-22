2adcac5466a844a27f5d2603aa173bf4068d1630
4drian3d <adriangonzalesval@gmail.com>
2023-02-22 04:39:19 +0000

    Fixed some issues

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -9,6 +9,15 @@ dependencies {
     compileOnly(libs.authlib)
 }
 
+repositories {
+    maven("https://repo.kryptonmc.org/releases")
+    maven("https://repo.opencollab.dev/maven-snapshots/")
+    maven("https://repo.viaversion.com/")
+    maven("https://repo.purpurmc.org/snapshots")
+    maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
+    maven("https://jitpack.io")
+}
+
 tasks.compileJava {
     options.release.set(17)
 }
